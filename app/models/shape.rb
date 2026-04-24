3bcad80bed9d7b128e6fa2b7d11dfe383c8dade2
pedrogdclima <pedrogdcl@gmail.com>
2026-04-24 14:10:26 -0400

    handle vehicles on added trips

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -5,6 +5,8 @@ class Shape < ApplicationRecord
 
     def self.for_vehicles(vehicles)
         trip_ids = vehicles.map{|vehicle| vehicle.trip_id}
+        # Remove trip_ids from vehicles with schedule_relationship: added
+        trip_ids.delete_if {|id| id.starts_with?('-')}
         shape_ids = Trip.find(trip_ids).map{|trip| trip.shape_id}.uniq
         self.find(shape_ids).map{|shape| shape.points.map{|point| [point.lat, point.lon]}}
     end
