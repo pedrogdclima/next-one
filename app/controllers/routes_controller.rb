8a780fadfe058b1c8aae539737f9cf5686df97d5
pedrogdclima <pedrogdcl@gmail.com>
2026-05-02 13:49:36 -0400

    remove unwanted ActiveRecord boilerplate actions

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,6 @@
 class RoutesController < ApplicationController
-  before_action :set_route, only: %i[ show edit update destroy save ]
+  # before_action :set_route, only: %i[ show edit update destroy save ]
+  before_action :set_route, only: %i[ show save ]
 
   # GET /routes or /routes.json
   def index
@@ -33,51 +34,51 @@ class RoutesController < ApplicationController
   end
 
   # GET /routes/new
-  def new
-    @route = Route.new
-  end
+  # def new
+  #   @route = Route.new
+  # end
 
   # GET /routes/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /routes or /routes.json
-  def create
-    @route = Route.new(route_params)
+  # def create
+  #   @route = Route.new(route_params)
 
-    respond_to do |format|
-      if @route.save
-        format.html { redirect_to @route, notice: "Route was successfully created." }
-        format.json { render :show, status: :created, location: @route }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @route.save
+  #       format.html { redirect_to @route, notice: "Route was successfully created." }
+  #       format.json { render :show, status: :created, location: @route }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @route.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /routes/1 or /routes/1.json
-  def update
-    respond_to do |format|
-      if @route.update(route_params)
-        format.html { redirect_to @route, notice: "Route was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @route }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @route.update(route_params)
+  #       format.html { redirect_to @route, notice: "Route was successfully updated.", status: :see_other }
+  #       format.json { render :show, status: :ok, location: @route }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @route.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /routes/1 or /routes/1.json
-  def destroy
-    @route.destroy!
+  # def destroy
+  #   @route.destroy!
 
-    respond_to do |format|
-      format.html { redirect_to routes_path, notice: "Route was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to routes_path, notice: "Route was successfully destroyed.", status: :see_other }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
