af1230ada64f2ec28da69c37b692e777c5dfc602
pedrogdclima <pedrogdcl@gmail.com>
2026-05-06 15:37:42 -0400

    add custom vehicle icon marker to map

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "leaflet", to: "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+pin "leaflet-rotatedmarker", to: "https://unpkg.com/leaflet-rotatedmarker@0.2.0/leaflet.rotatedMarker.js"
