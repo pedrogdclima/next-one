1d7b98866b97bb7cd27448893682512589e76a83
pedrogdclima <pedrogdcl@gmail.com>
2026-05-14 15:25:39 -0400

    remove unused rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :routes do
+  resources :routes, only: [:index, :show] do
     member do
       post :star
     end
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "routes#index"
 end
